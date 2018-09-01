91f8922f8b81f64645bd9b9465ac521489b8d98d
prodionov <pavelrodionov@hotmail.com>
2018-09-01 11:15:13 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Widget from './components/Widget';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Widget />
+      </div>
+    );
+  }
+}
+
+export default App;
